61155635259740fd6023c5277cba7c556e05ec90
Ibrahim Fiqhan <ifiqhan@gmail.com>
2025-04-20 16:25:32 +0700

    feat: integrate Tailwind CSS and remove unused assets
    - Added Tailwind CSS and its Vite plugin to the project dependencies.
    - Removed the unused vite.svg file from the public directory.
    - Updated App.css to include Tailwind CSS and removed legacy styles.
    - Refactored App.jsx to utilize Tailwind CSS for styling and removed unnecessary elements.
    - Cleaned up index.css by removing unused styles for links and buttons.
    - Updated vite.config.js to include Tailwind CSS plugin in the Vite configuration.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,26 +6,18 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <div className="flex flex-col justify-center items-center gap-4">
+        <div className='flex justify-center items-center'>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-60 text-amber-500">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 15a4.5 4.5 0 0 0 4.5 4.5H18a3.75 3.75 0 0 0 1.332-7.257 3 3 0 0 0-3.758-3.848 5.25 5.25 0 0 0-10.233 2.33A4.502 4.502 0 0 0 2.25 15Z" />
+          </svg>
+        </div>
+        <h1 className="text-amber-500">Weather App</h1>
+        <button className="bg-amber-500 py-2 px-4 rounded-lg hover:bg-transparent border-amber-500 border-2 text-white hover:text-amber-500 font-bold"
+          onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
