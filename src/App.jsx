8a37f22b3a6cb54855dfc0201052303f99b33657
Ibrahim Fiqhan <ifiqhan@gmail.com>
2025-04-26 20:28:55 +0700

    feat: update index.html title and restructure App component with Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,20 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div className="flex flex-col justify-center items-center gap-4">
-        <div className='flex justify-center items-center'>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-60 text-amber-500">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 15a4.5 4.5 0 0 0 4.5 4.5H18a3.75 3.75 0 0 0 1.332-7.257 3 3 0 0 0-3.758-3.848 5.25 5.25 0 0 0-10.233 2.33A4.502 4.502 0 0 0 2.25 15Z" />
-          </svg>
+      <div className='bg-white shadow-2xl rounded-2xl overflow-hidden'>
+        <div className='grid grid-cols-1 md:grid-cols-2'>
+          <div className='bg-gray-100 p-4'>
+            <h1 className='text-2xl font-bold'>Hello World</h1>
+            <p className='text-gray-700'>This is a simple React app with Tailwind CSS.</p>
+          </div>
+          <div className='bg-gray-200 p-4'>
+            <h2 className='text-xl font-semibold'>Tailwind CSS</h2>
+            <p className='text-gray-600'>This is a simple example of using Tailwind CSS with React.</p>
+          </div>
+
         </div>
-        <h1 className="text-amber-500">Weather App</h1>
-        <button className="bg-amber-500 py-2 px-4 rounded-lg hover:bg-transparent border-amber-500 border-2 text-white hover:text-amber-500 font-bold"
-          onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
       </div>
     </>
   )
